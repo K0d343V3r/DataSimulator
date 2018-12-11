514839398d982b5199ef30483495c015127ad657
K0d343V3r <zoolook@cox.net>
2018-12-11 10:00:19 -0800

    Interpolation now conforms to OPC HDA 1.2 Specification.

--- DataSimulator.Api/Helpers/DataGenerator.cs
+++ DataSimulator.Api/Helpers/DataGenerator.cs
@@ -20,13 +20,16 @@ namespace DataSimulator.Api.Helpers
             {
                 // start time is at whole second, generate Good/Raw value at this time
                 var value = GetValueAtTime(timePeriod.StartTime);
-                Quality quality = new Quality() { Major = value == null ? MajorQuality.Bad : MajorQuality.Good };
+                Quality quality = new Quality()
+                {
+                    Major = value == null ? MajorQuality.Bad : MajorQuality.Good
+                };
                 vqts.Add(new VQT(value, timePeriod.StartTime, quality));
             }
             else if (initialValue != InitialValue.None || (maxCount > 0 && projectedCount > maxCount))
             {
-                // initial value requested, or we have too many items, which require a value at start anyway
-                vqts.Add(CreateInterpolatedValue(timePeriod.StartTime, initialValue == InitialValue.SampleAndHold));
+                // initial value requested or we need to interpolate (requires value at initial time)
+                vqts.Add(InterpolateValueAt(timePeriod.StartTime, initialValue == InitialValue.SampleAndHold));
             }
 
             // add subsequent values
@@ -34,9 +37,8 @@ namespace DataSimulator.Api.Helpers
             long stepTicks;
             if (maxCount == 0 || projectedCount <= maxCount)
             {
+                // we need to generate raw values, start at next whole second
                 long fractionTicks = timePeriod.StartTime.Ticks % TimeSpan.TicksPerSecond;
-
-                // we start at first whole second
                 startTime = timePeriod.StartTime.AddTicks(TimeSpan.TicksPerSecond - fractionTicks);
 
                 // and generate one value per second
@@ -44,7 +46,7 @@ namespace DataSimulator.Api.Helpers
             }
             else
             {
-                // generate exactly maxCount values
+                // we need to generate interpolated values, divide time span to generate maxCount values
                 stepTicks = (timePeriod.EndTime - timePeriod.StartTime).Ticks / maxCount;
 
                 // skip value at start if we already have one
@@ -54,17 +56,32 @@ namespace DataSimulator.Api.Helpers
             while (startTime < timePeriod.EndTime)
             {
                 var value = GetValueAtTime(startTime);
-                Quality quality = new Quality() { Major = value == null ? MajorQuality.Bad : MajorQuality.Good };
-                if (startTime.Ticks % TimeSpan.TicksPerSecond > 0)
+                if (maxCount == 0 || projectedCount <= maxCount)
                 {
-                    // value is not at a second boundary, mark this as Interpolated
-                    quality.HDAQuality = HDAQuality.Interpolated;
+                    // not interpolating, use raw value
+                    Quality quality = new Quality()
+                    {
+                        Major = value == null ? MajorQuality.Bad : MajorQuality.Good
+                    };
+                    vqts.Add(new VQT(value, startTime, quality));
+                }
+                else if (value != null)
+                {
+                    // interpolating and we got a good value, use it
+                    Quality quality = new Quality()
+                    {
+                        // if value at second boundary, mark it as a raw value
+                        HDAQuality = startTime.Ticks % TimeSpan.TicksPerSecond == 0 ? HDAQuality.Raw : HDAQuality.Interpolated
+                    };
+                    vqts.Add(new VQT(value, startTime, quality));
+                }
+                else
+                {
+                    // interpolating and we got a bad value, interpolate non-bad
+                    vqts.Add(InterpolateValueAt(startTime, initialValue == InitialValue.SampleAndHold));
                 }
 
-                // add generated value
-                vqts.Add(new VQT(value, startTime, quality));
-
-                // and advance to next time
+                // advance to next time boundary
                 startTime = startTime.AddTicks(stepTicks);
             }
 
@@ -84,47 +101,75 @@ namespace DataSimulator.Api.Helpers
             return count;
         }
 
-        private VQT CreateInterpolatedValue(DateTime startTime, bool sampleAndHold)
+        //
+        // See OPC HDA 1.2 Specification, section 2.9.2.4.
+        // Reference: https://www.vegvesen.no/_attachment/1467098/binary/1124390
+        //
+        private VQT InterpolateValueAt(DateTime time, bool sampleAndHold)
         {
-            object value = null;
-            DateTime beforeTime = startTime.AddTicks(-(startTime.Ticks % TimeSpan.TicksPerSecond));
-            object beforeValue = GetValueAtTime(beforeTime);
-            if (beforeValue != null)
+            var quality = new Quality()
             {
-                if (sampleAndHold || !(beforeValue is float || beforeValue is double))
+                HDAQuality = HDAQuality.Interpolated
+            };
+
+            VQT beforeValue = FindNearestGoodValue(time, false);
+
+            if (sampleAndHold || !(beforeValue.Value is float || beforeValue.Value is double))
+            {
+                // for sample and hold, just use (hold) the before value
+                quality.Major = beforeValue.Quality.Major;
+                return new VQT(beforeValue.Value, time, quality);
+            }
+            else
+            {
+                VQT afterValue = FindNearestGoodValue(time, true);
+
+                double afterAsDouble = Convert.ToDouble(afterValue.Value);
+                double beforeAsDouble = Convert.ToDouble(beforeValue.Value);
+
+                // for linear, interpolate based on time/value ratio
+                long timeDifference = (time - beforeValue.Time).Ticks;
+                double valueDifference = afterAsDouble - beforeAsDouble;
+                double value = beforeAsDouble + timeDifference * valueDifference / TimeSpan.TicksPerSecond;
+
+                if (beforeValue.Quality.Major == MajorQuality.Uncertain || 
+                    afterValue.Quality.Major == MajorQuality.Uncertain)
                 {
-                    // for sample and hold, just use (hold) the before value
-                    value = beforeValue;
+                    quality.Major = MajorQuality.Uncertain;
+                }
+
+                if (beforeValue.Value is float)
+                {
+                    return new VQT(Convert.ToSingle(value), time, quality);
                 }
                 else
                 {
-                    DateTime afterTime = beforeTime.AddSeconds(1);
-                    object afterValue = GetValueAtTime(afterTime);
-                    if (afterValue != null)
-                    {
-                        double afterAsDouble = Convert.ToDouble(afterValue);
-                        double beforeAsDouble = Convert.ToDouble(beforeValue);
-
-                        // calculate value at start based on value/time ratio
-                        long timeDifference = (startTime - beforeTime).Ticks;
-                        double valueDifference = afterAsDouble - beforeAsDouble;
-                        double start = beforeAsDouble + timeDifference * valueDifference / TimeSpan.TicksPerSecond;
-                        if (beforeValue is float)
-                        {
-                            value = Convert.ToSingle(start);
-                        }
-                        else
-                        {
-                            value = start;
-                        }
-                    }
+                    return new VQT(value, time, quality);
                 }
             }
+        }
+
+        private VQT FindNearestGoodValue(DateTime time, bool forwards)
+        {
+            long fractionTicks = time.Ticks % TimeSpan.TicksPerSecond;
+            DateTime startTime = time.AddTicks(forwards ? TimeSpan.TicksPerSecond - fractionTicks : -fractionTicks);
+
+            bool skippedNonGood = false;
+            object value;
+            do
+            {
+                value = GetValueAtTime(startTime);
+                if (value == null)
+                {
+                    skippedNonGood = true;
+                    startTime = startTime.AddTicks(forwards ? TimeSpan.TicksPerSecond : -TimeSpan.TicksPerSecond);
+                }
+            }
+            while (value == null);
 
             var quality = new Quality()
             {
-                Major = value == null ? MajorQuality.Bad : MajorQuality.Good,
-                HDAQuality = HDAQuality.Interpolated
+                Major = skippedNonGood ? MajorQuality.Uncertain : MajorQuality.Good
             };
 
             return new VQT(value, startTime, quality);
@@ -135,7 +180,12 @@ namespace DataSimulator.Api.Helpers
             // align value to second boundary (assumes sample and hold)
             DateTime alignedTime = time.AddTicks(-(time.Ticks % TimeSpan.TicksPerSecond));
             var value = GetValueAtTime(alignedTime);
-            var quality = new Quality() { Major = value == null ? MajorQuality.Bad : MajorQuality.Good };
+
+            var quality = new Quality()
+            {
+                Major = value == null ? MajorQuality.Bad : MajorQuality.Good
+            };
+
             return new VQT(value, time, quality);
         }
 
